8707183c08c84459b540b9b49953f2a06841c842
Tiago Henrique <tiago_henrique@hotmail.com>
2025-06-18 11:19:13 -0300

    Update primeira_consulta.py

--- primeira_consulta.py
+++ primeira_consulta.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.set_page_config(layout='wide')
-url = ("https://lmu.famerp.br/tiago/indicadores.csv")
+url = ("https://lmu.famerp.br/dash/nutricao_paula/indicadores.csv")
 df = pd.read_csv(url)
 
 pc = df['data'].count()
